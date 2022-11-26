cdb039c46bd9fd9db24f177af04bd682810f8cb2
Anas jaidi <anas.jaidi@icloud.com>
2022-11-26 18:40:55 +0100

    add tarifs and intro to tarif page and fix spaces header

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,4 @@
 function activate(el: string | HTMLElement) {
-  console.log('activation running');
 	if (typeof el == 'string')
      el = <HTMLElement>document.querySelector(el)
 	el.classList.toggle("active");
--- src/scripts/spaces-menu.ts
+++ src/scripts/spaces-menu.ts
@@ -1,16 +1,20 @@
 function spaces() {
 	const head = document.querySelector(".fixed");
 	if (!head?.classList.contains("scrolled")) head?.classList.toggle("scrolled");
-	iconSwitch()
+	iconSwitch();
 	activate("header .spaces");
-	activate('.wrapper')
+	activate(".wrapper");
+	document.querySelector(".wrapper")?.addEventListener("mouseup", (e) => {
+		document.querySelector("header .spaces")?.classList.remove('active');
+		document.querySelector(".wrapper")?.classList.remove('active');
+		iconSwitch();
+	});
 }
 
 function Left() {
-  if (document.querySelector('.menu-page')?.classList.contains('active')) return menuPage()
-	activate('header .spaces')
-  iconSwitch()
-	activate('.wrapper')
+	if (document.querySelector(".menu-page")?.classList.contains("active"))
+		return menuPage();
+	activate("header .spaces");
+	iconSwitch();
+	activate(".wrapper");
 }
-
-
